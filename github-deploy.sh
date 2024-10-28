6a96bed037a841c6966d03b6e1121044eb2705bb
TimSpan <cat20201314@163.com>
2024-10-28 14:25:11 +0800

    Update github-deploy.sh

--- github-deploy.sh
+++ github-deploy.sh
@@ -2,17 +2,18 @@
 # 确保脚本抛出遇到的错误
 set -e
 # 生成静态文件
-# npm run build
+npm run build
 # 进入生成的文件夹
-# cd docs/.vuepress/dist
+cd dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 git init
-git status
 git add -A
-git commit -m 'deploy'
+git commit -m '暂时完结'
 # 如果发布到 https://<USERNAME>.github.io
-git push -f https://github.com/TimSpan/MyNeteaseMusic.git gh-pages
+git push -f git@github.com:TimSpan/MyNeteaseMusic.git gh-pages
+# git push -f git@github.com:TimSpan/MyNeteaseMusic.git master:gh-pages
+
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 # git push -f git@gitee.com:timspan/kblog.git master:gh-pages
